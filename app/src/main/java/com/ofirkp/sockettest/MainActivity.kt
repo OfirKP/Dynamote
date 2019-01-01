2875a4b7ef0e9c4ad09fe4416637eafe95b22dfd
OfirK <magicofir11@gmail.com>
2019-01-02 00:46:29 +0200

    This is my first commit.

--- app/src/main/java/com/ofirkp/sockettest/MainActivity.kt
+++ app/src/main/java/com/ofirkp/sockettest/MainActivity.kt
@@ -58,6 +58,7 @@ class MainActivity : AppCompatActivity() {
             c?.close()
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
